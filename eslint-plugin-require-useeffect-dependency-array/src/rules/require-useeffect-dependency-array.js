0e773ab25b9f30562e575be8816202f112a06b1d
Stoyan Kolev <stoyan.a.kolev@gmail.com>
2024-09-23 16:40:19 +0300

    add .gitattributes and format

--- eslint-plugin-require-useeffect-dependency-array/src/rules/require-useeffect-dependency-array.js
+++ eslint-plugin-require-useeffect-dependency-array/src/rules/require-useeffect-dependency-array.js
@@ -1,33 +1,33 @@
-"use strict";
+'use strict';
 
 /** @type {import('eslint').Rule.RuleModule} */
 module.exports = {
   meta: {
     messages: {
-      requireDependencyArray: "useEffect should have a dependency array.",
+      requireDependencyArray: 'useEffect should have a dependency array.',
     },
-    type: "problem",
+    type: 'problem',
     docs: {
-      description: "Enforce that useEffect has a dependency array.",
+      description: 'Enforce that useEffect has a dependency array.',
       recommended: true,
     },
-    fixable: "code",
+    fixable: 'code',
     schema: [],
   },
   create(context) {
     return {
       CallExpression(node) {
         if (
-          node.callee.type === "Identifier" &&
-          node.callee.name === "useEffect"
+          node.callee.type === 'Identifier' &&
+          node.callee.name === 'useEffect'
         ) {
           const dependencies = node.arguments[1];
-          if (!(dependencies && dependencies.type === "ArrayExpression")) {
+          if (!(dependencies && dependencies.type === 'ArrayExpression')) {
             context.report({
               node,
-              messageId: "requireDependencyArray",
+              messageId: 'requireDependencyArray',
               fix(fixer) {
-                return fixer.insertTextAfter(node.arguments[0], ", []");
+                return fixer.insertTextAfter(node.arguments[0], ', []');
               },
             });
           }
