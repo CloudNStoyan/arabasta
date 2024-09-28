7540d25d2f835be36b05eae4309b78c0f747d908
Hristo Kolev <hristo.a.kolev@gmail.com>
2024-09-29 00:11:53 +0300

    update rtl-jest config

--- eslint-config/src/rtl-jest.js
+++ eslint-config/src/rtl-jest.js
@@ -4,7 +4,6 @@ const testingLibrary = require('eslint-plugin-testing-library');
 module.exports = tseslint.config({
   extends: [testingLibrary.configs['flat/react']],
   rules: {
-    'testing-library/no-manual-cleanup': 'off',
     'testing-library/no-wait-for-multiple-assertions': 'off',
   },
 });
