3f414b974e8bbaafa1923e54827a21dd83c29ee8
Stoyan Kolev <stoyan.a.kolev@gmail.com>
2024-10-16 01:35:50 +0300

    refactor project setup

--- eslint-plugin-api-documentation/index.d.ts
+++ eslint-plugin-api-documentation/index.d.ts
@@ -7,4 +7,14 @@ export const configs: {
 
 export const rules: {
   'no-destructuring-arrays-as-objects': TSESLint.AnyRuleModule;
+  'require-example-decorator': TSESLint.AnyRuleModule;
+  'require-jsdoc-returns': TSESLint.AnyRuleModule;
+  'valid-alternative-response': TSESLint.AnyRuleModule;
+  'valid-alternative-response-type': TSESLint.AnyRuleModule;
+  'valid-security-decorator': TSESLint.AnyRuleModule;
+  'valid-example-decorator-type': TSESLint.AnyRuleModule;
+  'valid-response-decorator-type': TSESLint.AnyRuleModule;
+  'require-jsdoc-example-for-enum-type': TSESLint.AnyRuleModule;
+  'require-tags-metadata': TSESLint.AnyRuleModule;
+  'require-security-metadata': TSESLint.AnyRuleModule;
 };
