f58ee457a6997b6bdf4b613acce834e30bc4e449
Stoyan Kolev <stoyan.a.kolev@gmail.com>
2024-10-16 00:20:24 +0300

    add valid-response-decorator-type tests

--- eslint-plugin-api-documentation/src/rules/valid-response-decorator-type.test.ts
+++ eslint-plugin-api-documentation/src/rules/valid-response-decorator-type.test.ts
@@ -17,8 +17,81 @@ ruleTester.run(
   {
     // checks
     // 'valid' checks cases that should pass
-    valid: [],
+    valid: [
+      {
+        name: "when the response decorator has type arguments when 'allowedTypes' is set to 'any'",
+        code: normalizeIndent`
+        class Users {
+          @Response<AnythingWorks>(404)
+          getAllUsers() {}
+        }
+        `,
+        options: [
+          {
+            allowedTypes: 'any',
+          },
+        ],
+      },
+      {
+        name: "when the response decorator has the correct type arguments when 'allowedTypes' is set to custom types",
+        code: normalizeIndent`
+        class Users {
+          @Response<ApiError>(404)
+          getAllUsers() {}
+        }
+        `,
+        options: [
+          {
+            allowedTypes: ['ApiError'],
+          },
+        ],
+      },
+      {
+        name: "when the response decorator has status code that doesn't start with '4' or '5'",
+        code: normalizeIndent`
+        class Users {
+          @Response(204)
+          @Response(301)
+          getAllUsers() {}
+        }
+        `,
+      },
+    ],
     // 'invalid' checks cases that should not pass
-    invalid: [],
+    invalid: [
+      {
+        name: "when the response decorator doesn't have any type arguments",
+        code: normalizeIndent`
+        class Users {
+          @Response(404)
+          getAllUsers() {}
+        }
+        `,
+        errors: [
+          {
+            messageId: 'requiredTypeArguments',
+          },
+        ],
+      },
+      {
+        name: "when the response decorator has type arguments that are not in the 'allowedTypes'",
+        code: normalizeIndent`
+        class Users {
+          @Response<NotAllowedType>(404)
+          getAllUsers() {}
+        }
+        `,
+        options: [
+          {
+            allowedTypes: [],
+          },
+        ],
+        errors: [
+          {
+            messageId: 'firstTypeArgIsNotFromTheAllowedTypes',
+          },
+        ],
+      },
+    ],
   }
 );
