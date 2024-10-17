0f12c796a8db9f5b48bd91b9b9fe418aef1629fa
Stoyan Kolev <stoyan.a.kolev@gmail.com>
2024-10-17 10:58:58 +0300

    fix types and add the types check to the test script

--- eslint-plugin-api-documentation/index.d.ts
+++ eslint-plugin-api-documentation/index.d.ts
@@ -1,20 +1,23 @@
-import type { TSESLint } from '@typescript-eslint/utils';
+import { TSESLint } from '@typescript-eslint/utils';
 
-export const configs: {
-  'recommended-legacy': TSESLint.ClassicConfig.Config;
-  recommended: TSESLint.FlatConfig.Config;
+declare const plugin: TSESLint.FlatConfig.Plugin & {
+  configs: {
+    recommended: TSESLint.FlatConfig.Config;
+    'recommended-legacy': TSESLint.ClassicConfig.Config;
+  };
+  rules: {
+    'no-destructuring-arrays-as-objects': TSESLint.AnyRuleModule;
+    'require-example-decorator': TSESLint.AnyRuleModule;
+    'require-jsdoc-returns': TSESLint.AnyRuleModule;
+    'valid-alternative-response': TSESLint.AnyRuleModule;
+    'valid-alternative-response-type': TSESLint.AnyRuleModule;
+    'valid-security-decorator': TSESLint.AnyRuleModule;
+    'valid-example-decorator-type': TSESLint.AnyRuleModule;
+    'valid-response-decorator-type': TSESLint.AnyRuleModule;
+    'require-jsdoc-example-for-enum-type': TSESLint.AnyRuleModule;
+    'require-tags-metadata': TSESLint.AnyRuleModule;
+    'require-security-metadata': TSESLint.AnyRuleModule;
+  };
 };
 
-export const rules: {
-  'no-destructuring-arrays-as-objects': TSESLint.AnyRuleModule;
-  'require-example-decorator': TSESLint.AnyRuleModule;
-  'require-jsdoc-returns': TSESLint.AnyRuleModule;
-  'valid-alternative-response': TSESLint.AnyRuleModule;
-  'valid-alternative-response-type': TSESLint.AnyRuleModule;
-  'valid-security-decorator': TSESLint.AnyRuleModule;
-  'valid-example-decorator-type': TSESLint.AnyRuleModule;
-  'valid-response-decorator-type': TSESLint.AnyRuleModule;
-  'require-jsdoc-example-for-enum-type': TSESLint.AnyRuleModule;
-  'require-tags-metadata': TSESLint.AnyRuleModule;
-  'require-security-metadata': TSESLint.AnyRuleModule;
-};
+export = plugin;
